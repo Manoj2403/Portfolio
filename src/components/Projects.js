c956c97da5d24ffda1394fe07ee07b98fdabcfc4
MANOJ KUMAR V <manojvelusamy24@gmail.com>
2026-06-04 22:52:56 +0530

    Project Section Updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -74,71 +74,29 @@ const Projects = () => {
 
 
             {/* Main Container */}
-            <div className="flex items-center justify-center w-full overflow-x-auto py-10">
-                <div className="hidden lg:block w-0.5 h-72 bg-customGreen my-auto relative rounded-sm">
+            <div className="flex items-center justify-center w-full py-10">
+                <div className="w-0.5 h-72 bg-customGreen my-auto relative rounded-sm flex-shrink-0">
                     <div className="absolute inset-0 blur-sm bg-customGreen animate-pulse" />
                 </div>
-                <div
-                    className="relative h-[550px] mx-auto"
-                    style={{
-                        width: `${320 + (projects.length - 1) * 170}px`
-                    }}
-                >
+                <div className="grid grid-cols-1 gap-8 justify-items-center lg:hidden">
                     {projects.map((project, index) => (
-                        <div
-                            key={index}
-                            onMouseEnter={() => setActiveCard(index)}
-                            onMouseLeave={() => setActiveCard(null)}
-                            className={`
-                    absolute
-                    top-0
-                    w-[320px]
-                    h-[500px]
-                    rounded-2xl
-                    cursor-pointer
-                    flex
-                    flex-col
-                    border
-                    backdrop-blur-md
-                    transition-all
-                    duration-700
-                    ease-[cubic-bezier(0.22,1,0.36,1)]
-                    ${activeCard === index
-                                    ? "bg-white/15 border-customOrange/50 scale-[1.02] shadow-[0_25px_80px_rgba(255,255,255,0.15)]"
-                                    : "bg-white/10 border-white/10"
-                                }`}
-                            style={{
-                                left: (() => {
-                                    const normalPosition = index * 180;
-
-                                    if (activeCard === null) {
-                                        return `${normalPosition}px`;
-                                    }
-
-                                    if (index < activeCard) {
-                                        return `${normalPosition - 80}px`;
-                                    }
-
-                                    if (index > activeCard) {
-                                        return `${normalPosition + 80}px`;
-                                    }
-
-                                    return `${normalPosition}px`;
-                                })(),
-
-                                zIndex:
-                                    activeCard === index
-                                        ? 100
-                                        : projects.length - index
-                            }}
+                        <div key={index} className="bg-white/10
+                                        backdrop-blur-md
+                                        border
+                                        rounded-2xl
+                                        w-[320px]
+                                        h-[500px]
+                                        flex flex-col
+                                        cursor-pointer
+                                        transition-all
+                                        duration-300
+                                        hover:border-customOrange/50
+                                    "
                             onClick={() => {
                                 project.link
                                     ? window.open(project.link, "_blank")
-                                    : window.alert(
-                                        "No URL Found for " + project.name + "."
-                                    );
-                            }}
-                        >
+                                    : window.alert("No URL Found for " + project.name + ".");
+                            }}>
                             <div className="rounded-t-2xl overflow-hidden">
                                 <img
                                     src={project.image}
@@ -180,11 +138,6 @@ const Projects = () => {
                                     font-medium
                                     text-sm
                                     text-customGreen
-                                    transition-all
-                                    duration-300
-                                    hover:scale-105
-                                    hover:text-customPink
-                                    hover:border-customPink/50
                                 "
                                         >
                                             {skill}
@@ -195,7 +148,131 @@ const Projects = () => {
                         </div>
                     ))}
                 </div>
-                <div className="hidden lg:block w-0.5 h-72 bg-customGreen my-auto relative rounded-sm">
+
+                <div className="hidden lg:flex items-center justify-center w-full overflow-x-auto">
+
+
+                    <div
+                        className="relative h-[550px] mx-8"
+                        style={{
+                            width: `${320 + (projects.length - 1) * 180}px`
+                        }}
+                    >
+                        {projects.map((project, index) => (
+                            <div
+                                key={index}
+                                onMouseEnter={() => setActiveCard(index)}
+                                onMouseLeave={() => setActiveCard(null)}
+                                className={`
+                        absolute
+                        top-0
+                        w-[320px]
+                        h-[500px]
+                        rounded-2xl
+                        cursor-pointer
+                        flex
+                        flex-col
+                        border
+                        backdrop-blur-md
+                        transition-all
+                        duration-700
+                        ease-[cubic-bezier(0.22,1,0.36,1)]
+                        ${activeCard === index
+                                        ? "bg-white/15 border-customOrange/50 shadow-[0_25px_80px_rgba(255,255,255,0.15)]"
+                                        : "bg-white/10 border-white/10"
+                                    }
+                    `}
+                                style={{
+                                    left: (() => {
+                                        const normalPosition = index * 180;
+
+                                        if (activeCard === null) {
+                                            return `${normalPosition}px`;
+                                        }
+
+                                        if (index < activeCard) {
+                                            return `${normalPosition - 80}px`;
+                                        }
+
+                                        if (index > activeCard) {
+                                            return `${normalPosition + 80}px`;
+                                        }
+
+                                        return `${normalPosition}px`;
+                                    })(),
+                                    zIndex:
+                                        activeCard === index
+                                            ? 100
+                                            : projects.length - index
+                                }}
+                                onClick={() => {
+                                    project.link
+                                        ? window.open(project.link, "_blank")
+                                        : window.alert(
+                                            "No URL Found for " + project.name + "."
+                                        );
+                                }}
+                            >
+                                <div className="rounded-t-2xl overflow-hidden">
+                                    <img
+                                        src={project.image}
+                                        alt={`${project.name} screenshot`}
+                                        className="w-full h-44 object-cover block"
+                                    />
+                                </div>
+
+                                <div className="p-4 flex flex-col flex-1">
+                                    <div className="flex justify-between items-start gap-2">
+                                        <h1 className="text-sm text-white font-semibold break-words flex items-center gap-1">
+                                            {project.name}
+                                        </h1>
+
+                                        <p className="text-xs sm:text-sm text-gray-300 whitespace-nowrap font-cursive">
+                                            {project.projectDate}
+                                        </p>
+                                    </div>
+
+                                    <p className="text-sm text-customOrange mt-2 font-cursive">
+                                        {project.applicationType}
+                                    </p>
+
+                                    <p className="text-sm text-gray-300 mt-3">
+                                        {project.description || "Short summary..."}
+                                    </p>
+
+                                    <div className="flex flex-wrap gap-2 mt-auto pt-4">
+                                        {project.techStack.map((skill, skillIndex) => (
+                                            <div
+                                                key={skillIndex}
+                                                className="
+                                        rounded-xl
+                                        px-2
+                                        py-1
+                                        border
+                                        font-cursive
+                                        border-customGreen/50
+                                        font-medium
+                                        text-sm
+                                        text-customGreen
+                                        transition-all
+                                        duration-300
+                                        hover:scale-105
+                                        hover:text-customPink
+                                        hover:border-customPink/50
+                                    "
+                                            >
+                                                {skill}
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+
+                </div>
+                <div className="w-0.5 h-72 bg-customGreen my-auto relative rounded-sm flex-shrink-0">
                     <div className="absolute inset-0 blur-sm bg-customGreen animate-pulse" />
                 </div>
             </div>
